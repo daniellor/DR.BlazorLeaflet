f05a71a395dbfc9e8698032cd562987d14ca67f3
Daniel Romanowski <daniel@dotlink.pl>
2023-07-05 22:12:05 +0200

    NET MAUI sample added

---
+++ BlazorLeaflet/DR.BlazorLeaflet.Samples.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DR.BlazorLeaflet.Samples.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlazorLeaflet/DR.BlazorLeaflet.Samples.MAUI/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using DR.BlazorLeaflet.Samples.Data;
+using Microsoft.Extensions.Logging;
+
+namespace DR.BlazorLeaflet.Samples.MAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddScoped<CityService>();
+
+            return builder.Build();
+        }
+    }
+}
