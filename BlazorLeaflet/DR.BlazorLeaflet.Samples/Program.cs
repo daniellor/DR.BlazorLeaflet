ccf457cb1805493dc72d513392e56c8f5be0f109
Daniel Romanowski <daniel@dotlink.pl>
2023-07-05 21:20:25 +0200

    resorganized solution

--- BlazorLeaflet/DR.BlazorLeaflet.Samples/Program.cs
+++ BlazorLeaflet/DR.BlazorLeaflet.Samples/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace DR.BlazorLeaflet.Samples
+namespace DR.BlazorLeaflet.Samples.Server
 {
     public class Program
     {
